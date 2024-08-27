7f4d9c7e5eec2e4affa7998ee2720c44ca04279c
vietanhcao <vietanhcao1994@gmail.com>
2024-08-27 14:19:11 +0700

    feat(dish): add api use dompurify sanitize rich-text

--- src/apiRequests/dish.ts
+++ src/apiRequests/dish.ts
@@ -1,23 +1,18 @@
 import http from "../lib/http";
 import {
-	DishListResType,
-	AccountResType,
-	ChangePasswordBodyType,
 	CreateDishBodyType,
-	UpdateEmployeeAccountBodyType,
-	UpdateMeBodyType,
-} from "../schemaValidations/account.schema";
+	DishListResType,
+	DishResType,
+	UpdateDishBodyType,
+} from "../schemaValidations/dish.schema";
 
 const dishApiRequest = {
 	list: () => http.get<DishListResType>("/dishes"),
-	add: (body: CreateDishBodyType) =>
-		http.post<DishResType>("/dishes", body),
+	add: (body: CreateDishBodyType) => http.post<DishResType>("/dishes", body),
 	updateDish: (id: number, body: UpdateDishBodyType) =>
 		http.put<DishResType>(`/dishes/${id}`, body),
-	getDish: (id: number) =>
-		http.get<DishResType>(`/dishes/${id}`),
-	deleteDish: (id: number) =>
-		http.delete<DishResType>(`/dishes/${id}`),
+	getDish: (id: number) => http.get<DishResType>(`/dishes/${id}`),
+	deleteDish: (id: number) => http.delete<DishResType>(`/dishes/${id}`),
 };
 
 export default dishApiRequest;
