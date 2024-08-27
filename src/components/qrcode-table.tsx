e66c57ed2096b2a50bc20008f3ef769ddef0cff6
vietanhcao <vietanhcao1994@gmail.com>
2024-08-27 16:45:27 +0700

    feat(table): update content canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -7,7 +7,7 @@ import { getTableLink } from "../lib/utils";
 export default function QrcodeTable({
 	token,
 	tableNumber,
-	width,
+	width = 250,
 }: {
 	token: string;
 	tableNumber: number;
@@ -17,18 +17,31 @@ export default function QrcodeTable({
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		if (!canvas) return;
+		canvas.height = width + 70;
+		canvas.width = width;
+		const canvasContext = canvas.getContext("2d")!;
+		canvasContext.fillStyle = "#fff";
+		canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+		canvasContext.font = "20px Arial";
+		canvasContext.textAlign = "center";
+		canvasContext.fillStyle = "#000";
+		canvasContext.fillText(`Bàn số ${tableNumber}`, width / 2, width + 30);
+		canvasContext.fillText(`Quét mã QR để gọi món`, width / 2, width + 50);
+
+		const virtualCanvas = document.createElement("canvas");
+
 		QRCode.toCanvas(
-			canvas,
+			virtualCanvas,
 			getTableLink({
 				token,
 				tableNumber,
 			}),
 			function (error) {
 				if (error) console.error(error);
-				console.log("success!");
+				canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
 			}
 		);
-	}, [tableNumber, token]);
+	}, [tableNumber, token, width]);
 
 	return <canvas ref={canvasRef}></canvas>;
 }
