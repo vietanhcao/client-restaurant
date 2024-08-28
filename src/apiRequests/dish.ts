3347c20d59bd6b958e139e85f3cffdeccc9ed8ca
vietanhcao <vietanhcao1994@gmail.com>
2024-08-28 23:59:48 +0700

    chore: add note cache build and call api

--- src/apiRequests/dish.ts
+++ src/apiRequests/dish.ts
@@ -7,6 +7,8 @@ import {
 } from "../schemaValidations/dish.schema";
 
 const dishApiRequest = {
+	// Note: Nextjs 15 thì mặc định fetch sẽ là { cache: "no-cache" } dynamic rendering page
+	// hiệnt tại Nextjs 14 thì fetch mặc định là { cache: "force-cache" } static rendering page
 	list: () => http.get<DishListResType>("/dishes"),
 	add: (body: CreateDishBodyType) => http.post<DishResType>("/dishes", body),
 	updateDish: (id: number, body: UpdateDishBodyType) =>
