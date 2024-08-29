a46aa2bbd0f5d515c1b7a9b66e63b288b55f911e
vietanhcao <vietanhcao1994@gmail.com>
2024-08-30 01:45:27 +0700

    refactor: nav item with role

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,48 +1,88 @@
 "use client";
 
 import Link from "next/link";
-import { getAccessTokenFromLocalStorage } from "../../lib/utils";
+import {
+	cn,
+	getAccessTokenFromLocalStorage,
+	handleErrorApi,
+} from "../../lib/utils";
 import { use, useEffect, useState } from "react";
 import { useAppConext } from "../../components/app-provider";
+import { Role } from "../../constants/type";
+import { RoleType } from "../../types/jwt.types";
+import { useLogoutMutation } from "../../queries/useAuth";
+import { useRouter } from "next/navigation";
 
-const menuItems = [
+const menuItems: {
+	title: string;
+	href: string;
+	role?: RoleType[];
+	hideWhenLoggedIn?: boolean;
+}[] = [
 	{
-		title: "Món ăn",
-		href: "/menu",
+		title: "Trang chủ",
+		href: "/",
 	},
 	{
-		title: "Đơn hàng",
-		href: "/orders",
-		authRequired: true,
+		title: "Menu",
+		href: "/guest/menu",
+		role: [Role.Guest],
 	},
 	{
 		title: "Đăng nhập",
 		href: "/login",
-		authRequired: false,
+		hideWhenLoggedIn: true,
 	},
 	{
 		title: "Quản lý",
 		href: "/manage/dashboard",
-		authRequired: true,
+		role: [Role.Owner, Role.Employee],
 	},
 ];
 
 export default function NavItems({ className }: { className?: string }) {
-	const { isAuth } = useAppConext();
+	const router = useRouter();
+	const logoutMutation = useLogoutMutation();
+	const { setRole, role } = useAppConext();
 
-	return menuItems.map((item) => {
-		if (item.authRequired === false && isAuth) {
-			return null;
+	const handleLogout = async () => {
+		if (logoutMutation.isPending) return;
+		try {
+			await logoutMutation.mutateAsync();
+			router.push("/");
+			setRole(undefined);
+		} catch (error) {
+			handleErrorApi({
+				error,
+			});
 		}
+	};
 
-		if (item.authRequired === true && !isAuth) {
-			return null;
-		}
+	return (
+		<>
+			{menuItems.map((item) => {
+				// case đăng nhập hiển thị menu item đăng nhập
+				const isAuth = item.role && role && item.role.includes(role);
+				// case menu item có thể hiển thị dù đã đăng nhập hay chưa
+				const canShow =
+					(item.role === undefined && !item.hideWhenLoggedIn) ||
+					(item.hideWhenLoggedIn && !role);
 
-		return (
-			<Link href={item.href} key={item.href} className={className}>
-				{item.title}
-			</Link>
-		);
-	});
+				if (isAuth || canShow) {
+					return (
+						<Link href={item.href} key={item.href} className={className}>
+							{item.title}
+						</Link>
+					);
+				}
+
+				return null;
+			})}
+			{role && (
+				<div className={cn(className, "cursor-pointer")} onClick={handleLogout}>
+					Đăng xuất
+				</div>
+			)}
+		</>
+	);
 }
